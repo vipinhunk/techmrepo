88bb7a379d3e0857501ddc58e86a0cd2139ba4dd
vipinhunk <vipinhunkpal@gmail.com>
2022-10-23 13:24:12 +0530

    some chages application.java file in feature302 by anup

--- application.java
+++ application.java
@@ -17,3 +17,43 @@ public class Calculator {
 		 return result;
 		 
 	 }
+	 some changes by in file anup on 23/10/22
+	 
+	 package project1;
+
+import java.util.Scanner;
+
+public class Calculatorsolution {
+
+	
+
+	public static void main(String[] args) {
+		
+  System.out.println("execute start from the main method");
+Calculator calc = new Calculator();
+Scanner sc = new Scanner(System.in);
+
+int num1 = sc.nextInt();
+int num2 = sc.nextInt();
+
+int sum = calc.addition(num1, num2);
+System.out.println(sum);
+
+	
+	
+	int subtraction = calc.subtraction(num1,num2);
+	System.out.println(subtraction);
+	
+	
+	int multiply = calc.multiply(num1,num2);
+	System.out.println(multiply);
+	
+	
+	int divide = calc.divide(num1, num2);
+	System.out.println(divide);
+	
+	
+	}
+	
+	
+}
