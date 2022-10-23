634778559a500ffbbafa2ffa02405029687b965a
vipinhunk <vipinhunkpal@gmail.com>
2022-10-23 12:34:53 +0530

    application.java file added branch devlopement by dev1

---
+++ application.java
@@ -0,0 +1,19 @@
+package project1;
+
+public class Calculator {
+
+	int num1 ;
+	int num2 ;
+	
+	 int addition (int num1 ,int num2 ) {
+		 int result = num1 + num2 ;
+	     return result;
+	 }
+	 
+	 
+	 
+	  int subtraction (int num1, int num2) {
+		 int result = num1 - num2 ;
+		 return result;
+		 
+	 }
